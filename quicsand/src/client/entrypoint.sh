95b5392679521f404b5cdded7e5460f1f7d0148c
TiagoDuarte25 <tj.duarte@campus.fct.unl.pt>
2024-08-10 15:46:39 +0100

    feat: starting project restructuring and kollaps already incorporated running scripts manually

--- quicsand/src/client/entrypoint.sh
+++ quicsand/src/client/entrypoint.sh
@@ -3,15 +3,35 @@
 # Run build.sh in the background, redirecting output to /dev/null
 echo 'Starting client container...'
 #/app/quicsand/src/client/build.sh >/dev/null 2>&1
-/app/quicsand/src/client/build.sh $1
+/app/quicsand/src/client/build.sh msquic
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/build/implementations/msquic/build/bin/Release/
 
 server_ip=$(getent hosts server | awk '{ print $1 }')
 
-echo "----"
 echo "Server IP address: $server_ip"
-echo "----"
+
+trap 'exit 0' INT
+
+sleep 2
+
+echo $1 >> /tmp/log
+
+echo "Launched!"
+#host=$(hostname)
+#n=$((${#host}-1))
+host_id="$2"
+
+#obtain the service identifier
+service="$1-$KOLLAPS_UUID"
+
+echo ID $host_id >> /tmp/log
+echo Service $service >> /tmp/log
+
+#find out the IP of the servers through the experiment UUID 
+#nth client should connect to nth server
+server_ip_k=$(host $service | grep -oE '\d+\.\d+\.\d+\.\d+' | sort -u | sed -n ${host_id}p)
+echo SERVER_IP $server_ip_k >> /tmp/log
 
 # Start a shell to keep the container running
 ./bin/client $server_ip
--- quicsand/src/server/entrypoint.sh
+++ quicsand/src/server/entrypoint.sh
@@ -3,7 +3,7 @@
 # Run build.sh in the background, redirecting output to /dev/null
 echo 'Starting server container...'
 #/app/quicsand/src/server/build.sh >/dev/null 2>&1
-/app/quicsand/src/server/build.sh $1
+/app/quicsand/src/server/build.sh msquic
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/build/implementations/msquic/build/bin/Release/
 
