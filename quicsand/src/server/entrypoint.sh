9c3fa5a569ac2001100743e9ada63f6a2acd5c7a
TiagoDuarte25 <tj.duarte@campus.fct.unl.pt>
2024-09-18 19:04:09 +0100

    dockers running for msquic

--- quicsand/src/server/entrypoint.sh
+++ quicsand/src/server/entrypoint.sh
@@ -7,5 +7,9 @@ echo 'Starting server container...'
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/build/implementations/msquic/build/bin/Release/
 
+server_ip=$(getent hosts server | awk '{ print $1 }')
+
+echo "Server IP address: $server_ip"
+
 # Start server
-./bin/server
+./bin/server -c /app/quicsand/certs/quicsand-server.pem -k /app/quicsand/certs/key.pem -i "$server_ip" -p 4567
--- quicsand/src/server/quicsand_server.c
+++ quicsand/src/server/quicsand_server.c
@@ -15,10 +15,12 @@ int main(int argc, char *argv[])
 {
 	char *cert_path = NULL;
     char *key_path = NULL;
+	char *ip_address = NULL;
+	int port = 0;
     int opt;
 
     // Parse command-line arguments
-    while ((opt = getopt(argc, argv, "c:k:")) != -1) {
+    while ((opt = getopt(argc, argv, "c:k:i:p:")) != -1) {
         switch (opt) {
             case 'c':
                 cert_path = strdup(optarg);
@@ -26,16 +28,29 @@ int main(int argc, char *argv[])
             case 'k':
                 key_path = strdup(optarg);
                 break;
-            default:
-                fprintf(stderr, "Usage: %s -c <cert_path> -k <key_path>\n", argv[0]);
-                exit(EXIT_FAILURE);
+            case 'i':
+            	ip_address = strdup(optarg);
+            	break;
+			case 'p':
+				port = atoi(optarg);
+				break;
+			default:
+				fprintf(stderr, "Usage: %s -c <cert_path> -k <key_path> -i <ip_address> -p <port>\n", argv[0]);
+				exit(EXIT_FAILURE);
         }
     }
 
-    if (!cert_path || !key_path) {
-        fprintf(stderr, "Usage: %s -c <cert_path> -k <key_path>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    // Print the parsed options for debugging
+	printf("Certificate Path: %s\n", cert_path);
+	printf("Key Path: %s\n", key_path);
+	printf("IP Address: %s\n", ip_address);
+	printf("Port: %d\n", port);
+
+	// Ensure required options are provided
+	if (!cert_path || !key_path || !ip_address || port == 0) {
+		fprintf(stderr, "Usage: %s -c <cert_path> -k <key_path> -i <ip_address> -p <port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
 	FILE *fp;
 	fp = fopen("server.log", "w+");
@@ -50,7 +65,7 @@ int main(int argc, char *argv[])
 
 	context_t ctx = create_quic_context(cert_path, key_path);
 	fprintf(stderr, "Created context\n");
-	bind_addr(ctx, config->host, atoi(config->port));
+	bind_addr(ctx, ip_address, port);
 	fprintf(stderr, "Bound address\n");
 	set_listen(ctx);
 	fprintf(stderr, "Listening\n");
