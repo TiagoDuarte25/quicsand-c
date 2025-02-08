a2f86ee6476520e98c75322c25b6c65c825d64c3
tiagoduarte25 <tduartee@gmail.com>
2025-02-08 11:48:13 +0000

    fix: streaming leaks solved

--- quicsand/src/client/quicsand_client_streaming.c
+++ quicsand/src/client/quicsand_client_streaming.c
@@ -85,8 +85,12 @@ void *stream_data(void *args) {
     bin_to_hex(hash, hash_len, hash_hex);
     log_info("final hash: %s", hash_hex);
 
+    EVP_MD_CTX_free(mdctx);
+
     statistics_t stats;
     get_conneciton_statistics(ctx, connection, &stats);
+
+    close_connection(ctx, connection);
 
     fprintf(fp, "\n");
     fprintf(fp, "\n");
@@ -102,38 +106,32 @@ void *stream_data(void *args) {
     fprintf(fp, "total bytes sent: %ld\n", stats.total_sent_bytes);
     fprintf(fp, "total bytes received: %ld\n", stats.total_received_bytes);
 
+    destroy_quic_context(ctx);
+
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  char *ip_address = NULL;
-  char *file_path = NULL;
-  char *log_file = NULL;
-  int port = 0;
-  int duration = 0;
-  int data_size = 0;
-  int bitrate = 0;
-  int opt;
+    char *ip_address = NULL;
+    char *log_file = NULL;
+    int port = 0;
+    int duration = 0;
+    int bitrate = 0;
+    int opt;
 
-  fprintf(stdout, "quicsand client\n");
+    fprintf(stdout, "quicsand client\n");
 
-  // Parse command-line arguments
-  while ((opt = getopt(argc, argv, "i:p:f:d:s:l:b:")) != -1) {
-      switch (opt) {
+    // Parse command-line arguments
+    while ((opt = getopt(argc, argv, "i:p:d:l:b:")) != -1) {
+        switch (opt) {
             case 'i':
             ip_address = strdup(optarg);
             break;
             case 'p':
             port = atoi(optarg);
             break;
-            case 'f':
-            file_path = strdup(optarg);
-            break;
             case 'd':
             duration = atoi(optarg);
-            break;
-            case 's':
-            data_size = atoi(optarg);
             break;
             case 'l':
             log_file = strdup(optarg);
@@ -141,52 +139,50 @@ int main(int argc, char *argv[]) {
             case 'b':
             bitrate = atoi(optarg);
             break;
-          default:
+            default:
             fprintf(stdout, "usage: %s -i <ip_address> -p <port> [-f <file_path>]", argv[0]);
             exit(EXIT_FAILURE);
-      }
-  }
+        }
+    }
 
-  fprintf(stdout, "ip_address: %s\n", ip_address);
-  fprintf(stdout, "port: %d\n", port);
-  fprintf(stdout, "file_path: %s\n", file_path);
-  fprintf(stdout, "duration: %d\n", duration);
-  fprintf(stdout, "data_size: %d\n", data_size);
-  fprintf(stdout, "log_file: %s\n", log_file);
-  fprintf(stdout, "bitrate: %d\n", bitrate);
+    fprintf(stdout, "ip_address: %s\n", ip_address);
+    fprintf(stdout, "port: %d\n", port);
+    fprintf(stdout, "duration: %d\n", duration);
+    fprintf(stdout, "log_file: %s\n", log_file);
+    fprintf(stdout, "bitrate: %d\n", bitrate);
 
-  // Open the log file
-  FILE *fp = fopen(log_file, "w+");
-  if (!fp) {
-      perror("Failed to open log file");
-      return 1;
-  }
+    // Open the log file
+    FILE *fp = fopen(log_file, "w+");
+    if (!fp) {
+        perror("Failed to open log file");
+        return 1;
+    }
 
-  // Add file callback with the level
-  if (log_add_fp(fp, LOG_INFO) != 0) {
-      fprintf(fp, "Failed to add file callback\n");
-      return 1;
-  }
+    // Add file callback with the level
+    if (log_add_fp(fp, LOG_INFO) != 0) {
+        fprintf(fp, "Failed to add file callback\n");
+        return 1;
+    }
 
-  // Check if required arguments are provided
-  if (ip_address == NULL || port == 0) {
-      log_info("usage: %s -i <ip_address> -p <port> [-f <file_path>]", argv[0]);
-      fclose(fp);
-      return EXIT_FAILURE;
-  }
+    // Check if required arguments are provided
+    if (ip_address == NULL || port == 0) {
+        log_info("usage: %s -i <ip_address> -p <port> [-f <file_path>]", argv[0]);
+        fclose(fp);
+        return EXIT_FAILURE;
+    }
 
-  log_info("starting client");
+    log_info("starting client");
 
-  struct args *arguments = (struct args *)malloc(sizeof(struct args));
-  arguments->fp = fp;
-  arguments->ip_address = ip_address;
-  arguments->port = port;
-  arguments->file_path = file_path;
-  arguments->duration = duration;
-  arguments->bitrate = bitrate;
-  stream_data(arguments);
+    struct args *arguments = (struct args *)malloc(sizeof(struct args));
+    arguments->fp = fp;
+    arguments->ip_address = ip_address;
+    arguments->port = port;
+    arguments->duration = duration;
+    arguments->bitrate = bitrate;
+    stream_data(arguments);
 
-  free(ip_address);
-  free(file_path);
-  fclose(fp);
+    free(ip_address);
+    free(arguments);
+    free(log_file);
+    fclose(fp);
 }
